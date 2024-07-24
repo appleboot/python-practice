fe5a6c214b8f6e3dfa779bfa12e894d7f8c1eaeb
leo <g76553211235@gmail.com>
2024-07-24 16:08:18 +0800

    金字塔作業1

---
+++ 02_pyramid/main.py
@@ -0,0 +1,4 @@
+for i in range(20):
+    n_star = i*2+1
+    n_speace = 19-i
+    print(n_speace*" "+n_star*"*")
---
+++ 02_pyramid/test.py
@@ -0,0 +1,11 @@
+names = ["Wilson", "Kars", "Lisa", "Jostar", "Joseph", "Kira", "Dio"]
+
+print("Before swap: ", names)
+
+... # Your code
+
+names[0] = names[4]
+
+names[4] = names[0]
+
+print("After swap: ", names)
